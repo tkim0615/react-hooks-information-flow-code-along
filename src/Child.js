bc7f257ce5fea30f0a9fae97016589fb549bfa99
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-16 14:31:12 -0500

    practice

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, childColor }) {
+
+
+  return <div onClick={onChangeColor} className="child" style={{ backgroundColor: childColor }} />;
 }
 
 export default Child;
+//child is trying to change parent. so parent is passing down callback function which child can invoke when event happend in child.
--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,17 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childColor, setChildColor] = useState('#FFF');
 
+
+  function onChangeColor(){
+    setColor(randomColor)
+    setChildColor(getRandomColor())
+  }
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={onChangeColor} childColor={childColor}/>
+      <Child onChangeColor={onChangeColor} childColor={childColor}/>
     </div>
   );
 }
